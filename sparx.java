6753cea1e05261cd5bdf3e69f0046f613b5b7169
swapnil <swapnilghayal007@gmail.com>
2026-03-06 11:03:51 +0530

    java

---
+++ sparx.java
@@ -0,0 +1,46 @@
+public class sparx {
+    
+    public static void main(String args[]){
+
+        int matrix[][]={{1,2,3,4},
+                            {5,6,7,8},
+                            {9,10,11,12},
+                            {13,14,15,16}};
+
+        int startrow=0;
+        int endrow=matrix.length-1;
+        int startcol=0;
+        int endcol=matrix[0].length-1;
+
+        while(startrow<=endrow && startcol<=endcol)
+        {
+            for(int j=startcol; j<=endcol; j++){
+                System.out.println(matrix[startrow][j]);
+            }
+
+            for(int i=startrow+1; i<=endrow; i++)
+            {
+                System.out.println(matrix[i][endcol]);
+            }
+
+            for(int j=endcol-1; j>=startcol; j--)
+            {
+                System.out.println(matrix[endrow][j]);
+            }
+
+            for(int i=endrow-1; i>=startrow+1; i--)
+            {
+                System.out.println(matrix[i][startcol]);
+            }
+
+            startcol++;
+            startrow++;
+            endrow--;
+            endcol--;
+
+
+        }
+      
+
+    }
+}
